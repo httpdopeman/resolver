910cb65ca1e3d70343595d77871195f6b7646aab
httpdopeman <107885976+httpdopeman@users.noreply.github.com>
2023-05-17 16:12:36 +0530

    Update MFire.java
    Fixed MediaFire

--- jresolver/src/main/java/com/inside4ndroid/jresolver/sites/MFire.java
+++ jresolver/src/main/java/com/inside4ndroid/jresolver/sites/MFire.java
@@ -2,76 +2,32 @@ package com.inside4ndroid.jresolver.sites;
 
 import com.inside4ndroid.jresolver.Jresolver;
 import com.inside4ndroid.jresolver.model.Jmodel;
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import static com.inside4ndroid.jresolver.utils.Utils.putModel;
 
-import android.annotation.SuppressLint;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import android.content.Context;
-import android.os.Handler;
-import android.os.Looper;
-import android.webkit.JavascriptInterface;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 public class MFire {
 
-    @SuppressLint("StaticFieldLeak")
-    private static WebView webView;
-    private static boolean stopMMHandler = false;
 
-    @SuppressLint("SetJavaScriptEnabled")
-    public static void fetch(Context context, String url, final Jresolver.OnTaskCompleted onTaskCompleted) {
-        webView = new WebView(context);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().getAllowContentAccess();
-        webView.getSettings().setDomStorageEnabled(true);
-        webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
-        webView.addJavascriptInterface(new MyInterface(), "xGetter");
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                webView.evaluateJavascript(
-                        "(function() { return ('<html>'+document.getElementsByTagName('head')[0].innerHTML+'</html>'); })();",
-                        html -> {
 
-                            html = html.replaceAll("\\s", "");
 
-                            final String regex = "window\\.location\\.href.*?=.*?'(.*?)';";
-                            final Pattern pattern = Pattern.compile(regex);
-                            final Matcher matcher = pattern.matcher(html);
-                            if (matcher.find()) {
-                                ArrayList<Jmodel> jModels = new ArrayList<>();
-                                putModel(matcher.group(1),"Normal",jModels);
-                                onTaskCompleted.onTaskCompleted(jModels,false);
-                            }else {
-                                onTaskCompleted.onError();
-                            }
-                        });
-            }
-        });
+    public static void fetch(Context context, String url, final Jresolver.OnTaskCompleted onTaskCompleted) throws IOException {
+        Document document = Jsoup.connect(url).get();
+        Elements mfire = document.getElementsByClass("download_link");
+        String finalurl = document.getElementsByClass("input popsok").attr("href");
 
-        webView.loadUrl(url);
+        ArrayList<Jmodel>jModels = new ArrayList<>();
+        Jmodel jmodel = new Jmodel();
+        jmodel.setUrl(finalurl);
+        jmodel.setQuality("Normal");
+        onTaskCompleted.onTaskCompleted(jModels,false);
     }
 
-    private static class MyInterface {
-        @SuppressWarnings("unused")
-        @JavascriptInterface
-        public void error(final String error) {
-            new Handler(Looper.getMainLooper()).post(() -> {
-                destroyWebView();
-                stopMMHandler = true;
-            });
-        }
-    }
 
-    private static void destroyWebView() {
-        if (webView!=null) {
-            webView.loadUrl("about:blank");
-        }
-    }
 
 }
